0ac5a06278a9ebb6fcd8826b25e0f0f94b484772
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-11-18 20:12:58 +0530

    add build script

---
+++ fast.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+export KERNEL_DIR="/home/deepongi/Android/Singularity_Kernel"
+export KBUILD_OUTPUT="/home/deepongi/Android/Singularity_Kernel/out"
+export ZIP_DIR="/home/deepongi/Android/AnyKernel3"
+export ZIP_OUT_DIR="/home/deepongi/Android/Out_Zips"
+git submodule init
+git submodule update
+#make O=out mrproper
+#rm -rf out
+export PATH="/mnt/Hawai/toolchains/proton-clang-15/bin:$PATH"
+export USE_CCACHE=1
+export ARCH=arm64
+DTBIMAGE="dtb"
+export VARIANT="singularity-r01"
+export HASH=`git rev-parse --short=4 HEAD`
+export KERNEL_ZIP="$VARIANT-$HASH"
+export LOCALVERSION="~singularity-r01"
+make O=$KBUILD_OUTPUT CC=clang evo_defconfig
+make -j32 O=$KBUILD_OUTPUT CC=clang CROSS_COMPILE=/mnt/Hawai/toolchains/gcc-linaro-12.2.1-2023.01-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu- CROSS_COMPILE_ARM32=/mnt/Hawai/toolchains/gcc-linaro-12.2.1-2023.01_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
+cp -v $KBUILD_OUTPUT/arch/arm64/boot/Image $ZIP_DIR/Image
+cd $ZIP_DIR
+zip -r9 $VARIANT-$HASH.zip *
+mv -v $VARIANT-$HASH.zip $ZIP_OUT_DIR/
+echo -e "${green}"
+echo "-------------------"
+echo "Build Completed"
+echo "-------------------"
+echo -e "${restore}"
+echo "                                                     "
+echo "  _       __        _          _     _      _        "
+echo " (_)_ _  / _|___ __| |_ ___ __| |___| |__ _| |__ ___ "
+echo " | | ' \|  _/ -_) _|  _/ -_) _  |___| / _  | '_ (_-< "
+echo " |_|_||_|_| \___\__|\__\___\__,_|   |_\__,_|_.__/__/ "
+echo "                                                     "
+echo "                                                     "
+                                               
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
